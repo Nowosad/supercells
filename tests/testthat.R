0fe031430df94705368ac9587ac1349f758accf2
jn <tupiszakaczki@gmail.com>
2021-05-15 16:08:52 +0200

    reads tests data

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,14 +2,7 @@ library(testthat)
 library(supercells)
 library(terra)
 
-# 1 var
-# https://gist.github.com/btupper/8e8eb8c0ebf4402a3f87b5638eca954a
-v = rev(rast(volcano))
-ext(v) = c(2667400, 2668010, 6478705, 6479575)
-crs(v) = "EPSG:27200"
-names(v) = "elevation"
-
-# ttm()
-# qtm(v) + tm_grid()
+v1 = rast(system.file("raster/volcano.tif", package = "supercells"))
+v3 = rast(system.file("raster/ortho.tif", package = "supercells"))
 
 
