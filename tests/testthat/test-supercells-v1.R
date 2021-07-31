403f2cc8f3b3becf5cd92a65d702c02dfa81c9fc
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-07-31 13:52:41 +0200

    improves tests and coverages

--- tests/testthat/test-supercells-v1.R
+++ tests/testthat/test-supercells-v1.R
@@ -4,6 +4,7 @@ v1_c = supercells(v1, step = 8, compactness = 1)
 my_minarea = 3
 v1_d = supercells(v1, step = 8, compactness = 1, minarea = my_minarea)
 v1_e = supercells(v1, step = 8, compactness = 0.1, avg_fun = "median", dist_fun = "jensen_shannon")
+v1_f = supercells(v1, 100, compactness = 1, avg_fun = "mean")
 
 test_that("supercells works for 1 var", {
   expect_equal(ncol(v1_a), 5)
@@ -13,4 +14,5 @@ test_that("supercells works for 1 var", {
   expect_equal(nrow(v1_e), 88)
   expect_equal(v1_a, v1_c)
   expect_true(all(as.numeric(sf::st_area(v1_d)) >= xres(v1) * yres(v1) * my_minarea))
+  expect_equal(v1_a, v1_f)
 })
