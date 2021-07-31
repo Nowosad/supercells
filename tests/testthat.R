ec996d7027f72631062753867132cb35765b5fd2
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-07-31 11:50:12 +0200

    adds testchecks

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,3 +6,4 @@ v1 = rast(system.file("raster/volcano.tif", package = "supercells"))
 v3 = rast(system.file("raster/ortho.tif", package = "supercells"))
 # m = rast(system.file("raster/landcover2015.tif", package = "motif"))
 
+test_check("supercells")
