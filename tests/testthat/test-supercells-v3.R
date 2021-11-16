52b51e131852e3f61fe7cc8022c85d299cd8a6b7
Nowosad <tupiszakaczki@gmail.com>
2021-11-16 19:03:04 +0100

    updates v3 dataset

--- tests/testthat/test-supercells-v3.R
+++ tests/testthat/test-supercells-v3.R
@@ -3,6 +3,7 @@ v3_b = supercells(v3, 100, compactness = 1, transform = "to_LAB")
 
 test_that("supercells works for 3 var", {
   expect_equal(ncol(v3_a), 7)
-  expect_equal(nrow(v3_a), 80)
-  expect_equal(nrow(v3_b), 86)
+  expect_equal(nrow(v3_a), 79)
+  expect_equal(nrow(v3_b), 80)
+  expect_equal(as.numeric(st_bbox(v3_a)), as.numeric(st_bbox(v3)))
 })
