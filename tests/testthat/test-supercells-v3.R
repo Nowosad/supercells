50cecff9331cd407eaade899b581e40803e1dce0
Jakub Nowosad <tupiszakaczki@gmail.com>
2021-11-17 15:14:40 +0100

    cleans tests

--- tests/testthat/test-supercells-v3.R
+++ tests/testthat/test-supercells-v3.R
@@ -1,9 +1,10 @@
-v3_a = supercells(v3, 100, compactness = 1, chunks = 200)
+v3_a = supercells(v3, 100, compactness = 1)
 v3_b = supercells(v3, 100, compactness = 1, transform = "to_LAB")
 
 test_that("supercells works for 3 var", {
   expect_equal(ncol(v3_a), 7)
-  expect_equal(nrow(v3_a), 79)
-  expect_equal(nrow(v3_b), 80)
-  expect_equal(as.numeric(st_bbox(v3_a)), as.numeric(st_bbox(v3)))
+  expect_equal(nrow(v3_a), 80)
+  expect_equal(nrow(v3_b), 86)
+  expect_equal(as.numeric(sf::st_bbox(v3_a)), unname(as.vector(terra::ext(v3)))[c(1, 3, 2, 4)])
 })
+
