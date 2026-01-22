8d051f5a1d6671d300bca9ada8c1135286db6300
jn <tupiszakaczki@gmail.com>
2026-01-22 20:43:02 +0100

    cleans calc_stats

--- src/calc_stats.cpp
+++ src/calc_stats.cpp
@@ -1,13 +1,12 @@
 #include "calc_stats.h"
 
-double median(vector<double>& v){
+double median(std::vector<double>& v){
   size_t n = v.size() / 2;
   std::nth_element(v.begin(), v.begin()+n, v.end());
   return v[n];
 }
 
-double mean(vector<double>& v){
+double mean(const std::vector<double>& v){
   double sum = std::accumulate(v.begin(), v.end(), 0.0);
-  double mean = sum / v.size();
-  return mean;
+  return sum / v.size();
 }
